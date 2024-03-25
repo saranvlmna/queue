4d3cac1cf2e4cade975723c058f48a1a8038bac0
Saranjith <saranjith@metrictreelabs.com>
2024-03-25 11:43:54 +0530

    queue setup

---
+++ queue_service/consumer.js
@@ -0,0 +1,34 @@
+var amqp = require("amqplib/callback_api");
+
+amqp.connect("amqp://localhost", function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function (error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+    ////
+    // connect creted queue
+    var queue = "newTask";
+    channel.assertQueue(queue, {
+      durable: true,
+    });
+    //fetch tasks in queue
+    channel.consume(
+      queue,
+      function (task) {
+        //proccess and next
+        var secs = 10;
+        console.log(" [x] Received task %s", task.content.toString());
+        setTimeout(function () {
+          console.log(" [x] Done");
+        }, secs * 1000);
+      },
+      {
+        noAck: true,
+      }
+    );
+    ////
+  });
+});
---
+++ queue_service/create_queue.js
@@ -0,0 +1,30 @@
+var amqp = require("amqplib/callback_api");
+//create new queue
+var queue = "newTask";
+
+amqp.connect("amqp://localhost", function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  //create a new chanel
+  connection.createChannel(function (error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+    ////
+    channel.assertQueue(queue, {
+      durable: true,
+    });
+    //push tasks to queue
+    var task = "Hello Server!";
+    channel.sendToQueue(queue, Buffer.from(task), {
+      persistent: true,
+    });
+    console.log(" [x] task pushed to queue '%s'", task);
+    ///
+  });
+  setTimeout(function () {
+    connection.close();
+    process.exit(0);
+  }, 500);
+});
